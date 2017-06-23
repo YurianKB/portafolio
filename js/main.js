ed93a9b882795cdbdd5cc57215421a513a6a938b
YurianKB <yuriankb@gmail.com>
2017-06-23 12:57:58 -0500

    Primer commit  con maquetado y empezando JQuery, sin  funcionalidad aún y sin responsive

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.Panel').hide();
+    $('.deslizar').click(function(){
+        $('.Panel').slideToggle('slow');
+   });
+    $('.Panel2').hide();
+    $('.deslizar2').click(function(){
+        $('.Panel2').slideToggle('slow');
+   });
+    $('.Panel3').hide();
+    $('.deslizar3').click(function(){
+        $('.Panel3').slideToggle('slow');
+   });
+    $('.Panel4').hide();
+   	$('.deslizar4').click(function(){
+        $('.Panel4').slideToggle('slow');
+   });
+});
